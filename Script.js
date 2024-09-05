2349d7f4a9b69e932f851c8de8168eba24869d4f
Alvaro-R-Martinez <alvaro.r.s.martinez@proton.me>
2024-09-05 20:56:48 -0300

    registro do SW

--- Script.js
+++ Script.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(registration => {
+      console.log('Service Worker registrado com sucesso:', registration);
+    })
+    .catch(error => {
+      console.log('Falha ao registrar o Service Worker:', error);
+    });
+}
+
 document.getElementById('login-btn').addEventListener('click', function(event) {
     event.preventDefault(); 
 
