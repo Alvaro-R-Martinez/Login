d06d87bbce1df1797ff038a2fbf05b0d57c0193a
Alvaro-R-Martinez <alvaro.r.s.martinez@proton.me>
2024-09-05 20:16:14 -0300

    retiranda de comentarios

--- Script.js
+++ Script.js
@@ -1,22 +1,19 @@
 document.getElementById('login-btn').addEventListener('click', function(event) {
-    event.preventDefault(); // Impede o envio do formulário
-  
-    // Verifica se os campos estão preenchidos
+    event.preventDefault(); 
+
     const username = document.querySelector('input[type="text"]').value;
     const password = document.querySelector('input[type="password"]').value;
   
     if (username && password) {
-      // Verifica se a API de Notificações está disponível
       if ('Notification' in window) {
         Notification.requestPermission().then(permission => {
           if (permission === 'granted') {
-            // Exibe a notificação de login com sucesso
             navigator.serviceWorker.ready.then(function(registration) {
               registration.showNotification('Login feito com sucesso!', {
                 body: 'Você está logado como ' + username,
-                icon: '/icons/icon-192x192.png', // Caminho para o ícone
-                vibrate: [200, 100, 200], // Vibração ao receber a notificação
-                tag: 'login-notification' // Identificador da notificação
+                icon: '/icons/icon-192x192.png',
+                vibrate: [200, 100, 200], 
+                tag: 'login-notification'
               });
             });
           } else {
